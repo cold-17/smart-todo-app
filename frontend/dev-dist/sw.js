87ea96345ec3d6865f73be335a6169d29c2461aa
Charlie Old <charlie.old1706@gmail.com>
2025-11-10 18:35:38 +0000

    ready for VPS Deploy

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.odpl7ta6pco"
+    "revision": "0.0n0nri57e2g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
